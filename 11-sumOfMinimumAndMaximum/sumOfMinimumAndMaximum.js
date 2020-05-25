e0c4fc0e7d0495508c5ef0b53a48f7f03b40425e
Ivan Lei <ilei0893@gmail.com>
2020-05-25 13:11:20 -0400

    Added sum to get sum of minimum and maximum nums of an array

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,18 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  var sum = 0, min = nums[0], max = nums[0];
+  for(var i = 0; i < nums.length; i++)
+  {
+    if(nums[i + 1] < min)
+    {
+      min = nums[i + 1];
+    }
+    if(nums[i + 1] > max)
+    {
+      max = nums[i + 1];
+    }
+  }
+  sum = min + max;
+  return sum;
 }
 
 // Do not edit this line;
