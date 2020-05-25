80ae217d5bae61b336f43a6e85b6a8b2c0674ec6
Ivan Lei <ilei0893@gmail.com>
2020-05-24 23:24:23 -0400

    Added Solution

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,11 +1,12 @@
 function titleCaseEdit(title) {
-  var splitStr = title.toLowerCase().split(' ');
-  for(var i = 0; i < splitStr.length; i++)
+  var splitStr = title.toLowerCase().split(' ');      //sets splitStr to lower case and split it by whitespace.
+  console.log(splitStr);
+  for(var i = 0; i < splitStr.length; i++)  
   {
-      splitStr[i]= splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+      splitStr[i]= splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);    //Make char at index 0 and appends rest of the string
   }
   return splitStr.join(' ');
 }
-
+    
 // Do not edit this line;
 module.exports = titleCaseEdit;
