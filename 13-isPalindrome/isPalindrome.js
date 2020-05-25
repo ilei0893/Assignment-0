39d55af921e2db222f42a7089c40d746cb752be8
Ivan Lei <ilei0893@gmail.com>
2020-05-25 17:57:34 -0400

    Added checker to check if word is a palindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,15 @@
 function isPalindrome(word) {
-  // Insert code here;
+  var palindrome = "";      //Initialize empty string to add word in reverse
+  var bool = false;         //Default words are not palindromes
+  for(var i = word.length - 1; i >= 0; i--)   //Begin at end of word, add it to palindrome, and decrement
+  {
+    palindrome += word[i];
+  }
+  if(palindrome === word)       //If word is the same as palindrome, is palindrome
+  {
+    bool = true;
+  }
+  return bool;
 }
 
 // Do not edit this line;
