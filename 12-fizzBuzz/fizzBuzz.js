27853deb7ab99f3385f94e7476d3de72cbdef55d
Ivan Lei <ilei0893@gmail.com>
2020-05-25 16:39:27 -0400

    Added array to return indices that follow FizzBuzz rules

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,21 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  //Initialize array to be filled
+  var arr = [];   
+
+  for (var i = start; i <= end; i++) {
+
+    if ((i % 5 == 0) && (i % 3 == 0)) {   //If divisible by 3 and 5 = FizzBuzz
+      arr.push("FizzBuzz");
+    } else if (i % 3 == 0) {       //If divisible by 3 == Fizz
+      arr.push("Fizz");
+    }
+    else if (i % 5 == 0) {         //If divisible by 5 == Buzz
+      arr.push("Buzz");
+    } else {                   //If none of the above == index #
+      arr.push(i);
+    }
+  }
+  return arr;
 }
 
 // Do not edit this line;
