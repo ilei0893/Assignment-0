73ef725900eddaac8df64aab1ec4591d5e2bc5ff
Ivan Lei <ilei0893@gmail.com>
2020-05-25 02:47:10 -0400

    Added counter to count num of numbers that match the index

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,12 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  var counter = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if(nums[i] == i)
+    {
+      counter++;
+    }
+  }
+  return counter;
 }
 
 // Do not edit this line;
