21c160655ae9a1261e343f9b941dea540fccb600
Ivan Lei <ilei0893@gmail.com>
2020-05-25 18:42:32 -0400

    Create a checker to count how many instances of a letter occur

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,18 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let map = {};
+
+  for (var i = 0; i < word.length; i++) {
+    var key = word.charAt(i);
+
+    if (map[key]) {
+      map[key]++;
+    }
+    else {
+      map[key] = 1;
+    }
+
+  }
+  return map;
 }
 
 // Do not edit this line;
