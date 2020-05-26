000f63def73db376586e9b58f313544311534275
Ivan Lei <ilei0893@gmail.com>
2020-05-25 20:27:02 -0400

    Added solutions for exercise 14 and 15

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,18 +1,18 @@
 function frequencyCounter(word) {
-  let map = {};
+  let arr = {}; 
 
   for (var i = 0; i < word.length; i++) {
     var key = word.charAt(i);
 
-    if (map[key]) {
-      map[key]++;
+    if (arr[key]) {   //Check if key exists and increment if it does
+      arr[key]++;
     }
-    else {
-      map[key] = 1;
+    else {            //If key does not exist, set new key with value of 1
+      arr[key] = 1;
     }
 
   }
-  return map;
+  return arr;
 }
 
 // Do not edit this line;
