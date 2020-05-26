3bb29311898c53b26400ad74f6483432c6b8f984
Ivan Lei <ilei0893@gmail.com>
2020-05-26 16:03:19 -0400

    Added Throws statement Added checker to see if two numbers in an array add up to the specified target

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,24 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  let equalsTarget = false;
+  let sum = 0;
+
+ 
+  if(nums.length <= 1)
+  {
+    throw "Please enter an array of atleast 2 numbers";
+  }
+  for(var i = 0; i < nums.length; i++)
+  {
+      for(var j = 0; j < nums.length; j++)
+      {
+          sum = nums[i] + nums[j];
+          if(sum == target)
+          {
+            equalsTarget = true;
+          }
+      }
+  }
+  return equalsTarget;
 }
 
 // Do not edit this line;
