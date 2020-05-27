5094ea0688953b2a9fdfcac2aa42a0b6aa9dcd92
Ivan Lei <ilei0893@gmail.com>
2020-05-26 20:48:01 -0400

    Implemented binary search to check if a target exists in an array

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -2,10 +2,27 @@ class MySolution {
   constructor() {
     this.flag = false;
   }
-
   binarySearch(nums, target) {
-    // Insert code here;
+
+    let start = 0;              //Start Point
+    let end = nums.length - 1;    //End Point
+
+    while (start <= end) {
+
+      let mid = Math.floor((start + end) / 2);
+
+      if (nums[mid] === target) {             //If mid equals target, then return true.
+        return true;
+      } else if (nums[mid] < target) {        //Else if mid number is less than target, use right sub array
+        start = mid + 1;
+      }
+      else {                                  //Else use left sub array
+        end = mid - 1;
+      }
+    }
+    return false;
   }
+
 }
 
 // Do not edit this line;
